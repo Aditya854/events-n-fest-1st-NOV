047e8ccc5f07f7300558843f71f9fb23b5ceb598
Aditya Aupta <guptaaditya854@gmail.com>
2021-11-01 00:07:43 +0530

    .changes by 1st NOV

---
+++ main.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+
+        var height = $('.first-container').height();
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop >= height - 620) {
+            $('.nav-container').addClass('solid-nav');
+        } else {
+            $('.nav-container').removeClass('solid-nav');
+        }
+
+    });
+});
+
+
+/*http://callmenick.com/post/expanding-search-bar-using-css-transitions*/
+(function($) {
+    "use strict";
+  
+    var $navbar = $(".nav"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+
+    //scroll top 0 sticky
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        if (scrollTop > 5) {
+          $navbar.addClass("sticky");
+        } else {
+          $navbar.removeClass("sticky");  
+        }
+    });
+    
+    //section sticky
+    /*$(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        if ($(window).height() > scrollTop) {
+          $navbar.removeClass("sticky");
+        } else {
+          $navbar.addClass("sticky");  
+        }
+    });*/
+
+})(jQuery, undefined);
+
+$(".menu").click(function(){
+  $("#nav").toggleClass("open");
+});
